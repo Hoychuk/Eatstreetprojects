fdbf387b9e055b0f42359df2d6db3ad7a730ea1a
DeadShank <rigenkih1996@gmail.com>
2026-04-30 11:37:02 +0300

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,65 +1,22 @@
-import { PlaywrightTestConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
-const commonConfig = {
-  use: {
-    ...devices["Desktop Chrome"],
-    channel: "chrome",
-  },
-};
-
-const projects = [
-  {
-    name: "chrome 1366 768",
-    use: {
-      ...commonConfig.use,
-      viewport: { width: 1366, height: 768 },
-    },
-    workers: 1,
-    retries: 0,
-    retryIntervals: [1000, 2000],
-    slowMo: 0,
-  },
-];
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-
-const config: PlaywrightTestConfig = {
-  projects,
+export default defineConfig({
+  timeout: 90000,
+  workers: 1,
+  retries: 0,
   use: {
     headless: false,
     screenshot: "only-on-failure",
     video: { mode: "retain-on-failure", size: { width: 640, height: 360 } },
   },
-  timeout: 90000,
-};
-
-
-export default config;
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        channel: "chrome",
+        viewport: { width: 1366, height: 768 },
+      },
+    },
+  ],
+});
